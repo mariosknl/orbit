4d042617158ea479063c288c7902069ed1254f5b
mariosknl <mariosknl@outlook.com>
2020-12-19 21:04:25 +0200

    navigate conditionally based on auth state

--- orbit-app/src/context/AuthContext.js
+++ orbit-app/src/context/AuthContext.js
@@ -25,11 +25,19 @@ const AuthProvider = ({ children }) => {
     });
   };
 
+  const isAuthenticated = () => {
+    if (!authState.token || !authState.expiresAt) {
+      return false;
+    }
+    return new Date().getTime() / 1000 < authState.expiresAt;
+  };
+
   return (
     <Provider
       value={{
         authState,
         setAuthState: (authInfo) => setAuthInfo(authInfo),
+        isAuthenticated,
       }}
     >
       {children}
